1e5b6b24cde449bd4d15178d25b23b025d3825e5
Alvaro L <82913629+ALoran141@users.noreply.github.com>
2022-05-11 21:12:45 -0600

    added player class file

---
+++ player.py
@@ -0,0 +1,29 @@
+'''
+Class Author: Alvaro Loran 
+Description: This player class will keep track of the points and player guesses.
+'''
+
+
+class Player:
+    def __init__(self):
+        self.score = 300
+        self.currentGuess = int
+        self.prevGuess = int
+
+    
+    def showPoints(self):
+        print(self.score)
+
+    def updatePrevGuess(self):
+        self.prevGuess = self.currentGuess
+
+    def newGuess(self, newGuess):
+        self.updatePrevGuess()
+        self.currentGuess = newGuess
+
+    def addPoints(self):
+        self.score += 100
+
+
+    def removePoints(self):
+        self.score -= 75
